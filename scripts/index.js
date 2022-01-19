fd6d827079a355df4c3bedd00849e828379b197c
Stas Makarov <makarov.s.d@yandex.ru>
2022-01-19 23:35:39 +0300

    fix styles

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,24 @@
 const profileEditButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = document.querySelector('.popup__close');
 const popup = document.querySelector('.popup');
+const formElement = document.querySelector('.popup__form');
 
-profileEditButton.addEventListener('click', function (evt){
-    evt.preventDefault()
+profileEditButton.addEventListener('click', function() {
     popup.classList.add('popup__open');
 });
 
-popupCloseButton.addEventListener('click', function (){
+popupCloseButton.addEventListener('click', function() {
     popup.classList.remove('popup__open');
 });
 
-
-
-let formElement = document.querySelector('.popup__form');
-let nameInput = formElement.querySelector('[name="name"]');
-let jobInput = formElement.querySelector('[name="job"]');
-
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-
-    // Получите значение полей jobInput и nameInput из свойства value
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
-
-    let name = document.getElementsByClassName('.profile__heading-text')
-    nameInput.value.add.name.textContent;
-
-
-    document.querySelector('.profile__heading-text') = nameInput.value.textContent; 
-    jobInput.value.textContent 
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    const data = new FormData(evt.target);
+    let nameReplace = document.querySelector('.profile__heading-text');
+    let jobReplace = document.querySelector('.profile__paragraph-text');
+     nameReplace.textContent = data.get('name');
+    jobReplace.textContent = data.get('job');
+    popup.classList.remove('popup__open');
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler); 

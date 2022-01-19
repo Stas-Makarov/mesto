7acb2bce332284fb25d05085281b802af706a91c
Stas Makarov <makarov.s.d@yandex.ru>
2022-01-19 16:06:52 +0300

    field: file readme.md

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,4 @@
 const profileEditButton = document.querySelector('.profile__edit-button');
-const profileAddButton = document.querySelector('.profile__add-button');
 const popupCloseButton = document.querySelector('.popup__close');
 const popup = document.querySelector('.popup');
 
@@ -8,11 +7,33 @@ profileEditButton.addEventListener('click', function (evt){
     popup.classList.add('popup__open');
 });
 
-profileAddButton.addEventListener('click', function (evt){
-    evt.preventDefault()
-    popup.classList.add('popup__open');
-});
-
 popupCloseButton.addEventListener('click', function (){
     popup.classList.remove('popup__open');
 });
+
+
+
+let formElement = document.querySelector('.popup__form');
+let nameInput = formElement.querySelector('[name="name"]');
+let jobInput = formElement.querySelector('[name="job"]');
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+
+    // Получите значение полей jobInput и nameInput из свойства value
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+
+    let name = document.getElementsByClassName('.profile__heading-text')
+    nameInput.value.add.name.textContent;
+
+
+    document.querySelector('.profile__heading-text') = nameInput.value.textContent; 
+    jobInput.value.textContent 
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler); 

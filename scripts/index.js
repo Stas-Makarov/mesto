612de4754637acf0eeda974520c2456b8505f501
Stas Makarov <makarov.s.d@yandex.ru>
2022-01-26 14:19:47 +0300

    fix: review remarks

--- scripts/index.js
+++ scripts/index.js
@@ -3,22 +3,29 @@ const popupCloseButton = document.querySelector('.popup__close');
 const popup = document.querySelector('.popup');
 const formElement = document.querySelector('.popup__form');
 
-profileEditButton.addEventListener('click', function() {
-    popup.classList.add('popup__open');
-});
+let name = document.querySelector('.profile__heading-text');
+let job = document.querySelector('.profile__paragraph-text');
 
-popupCloseButton.addEventListener('click', function() {
+
+function popupOpen() {
+    const inputs = formElement.elements;
+    inputs['name'].value =  name.textContent;
+    inputs['job'].value = job.textContent;
+    popup.classList.add('popup__open');
+}
+
+function popupClose() {
     popup.classList.remove('popup__open');
-});
+}
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
     const data = new FormData(evt.target);
-    let nameReplace = document.querySelector('.profile__heading-text');
-    let jobReplace = document.querySelector('.profile__paragraph-text');
-     nameReplace.textContent = data.get('name');
-    jobReplace.textContent = data.get('job');
-    popup.classList.remove('popup__open');
+    name.textContent = data.get('name');
+    job.textContent = data.get('job');
+    popupClose();
 }
 
+profileEditButton.addEventListener('click', popupOpen);
+popupCloseButton.addEventListener('click', popupClose);
 formElement.addEventListener('submit', formSubmitHandler); 
